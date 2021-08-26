e31d0d3a2f92167133aa1c085b9e36941b831187
Gilda A <gilda.ahmadniaye@gmail.com>
2021-08-26 21:50:59 +0200

    updated buttons

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,22 @@
 import "./about.css";
 import AboutCards from "./AboutCards";
+import { Link } from "react-router-dom";
 
 const About = () => {
     return (
         <div className="about-container">
+                       <div></div>
+            <p>
+            Ett samarbete mellan Yrgo och Räddningsmissionen i Göteborg<br></br>
+            En digital berättelse skapad av:
+            </p>
+            <div></div>
             <AboutCards></AboutCards>
+            <Link className="homeBtn" to="/">
+            Back
+          </Link>
         </div>
+        
     );
 };
 
