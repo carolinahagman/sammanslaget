50217f1c250b94cc002cf1566cb485bd1c344dac
Gilda A <gilda.ahmadniaye@gmail.com>
2021-08-26 08:44:02 +0200

    added basic about

---
+++ src/components/About.js
@@ -0,0 +1,22 @@
+import "./about.css";
+
+const About = () => {
+    return (
+        <div className="container-about">
+            <h1>The Team Behind The Website</h1>
+            <span>About Us</span>
+            <div className="contact">
+                <h2>Name</h2>
+                <span>email?</span>
+                <p>Lorem ipsum lol</p>
+            </div>
+            <div className="contact">
+                <h2>Name</h2>
+                <span>email?</span>
+                <p>Lorem ipsum lol</p>
+            </div>
+        </div>
+    );
+};
+
+export default About;
