be6dc81cc7002ecd5e5dccb46c2df5320e1f55cc
Gilda A <gilda.ahmadniaye@gmail.com>
2021-08-26 11:25:29 +0200

    updated about stuff

--- src/components/About.js
+++ src/components/About.js
@@ -1,21 +1,62 @@
 import "./about.css";
 
+const groups = [
+    {
+        index: 1,
+        title: "Game Artist",
+        name: "Jimmy",
+        mail: "mail",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    {
+        index: 2,
+        title: "Game Programmer",
+        name: "Johan",
+        mail: "mail",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    {
+        index: 3,
+        title: "Manusförfattare",
+        name: "Alexandra",
+        mail: "mail",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    {
+        index: 4,
+        title: "UX",
+        name: "Malin",
+        mail: "mail",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    {
+        index: 5,
+        title: "Webbutvecklare",
+        name: "Carolina",
+        mail: "mail",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    {
+        index: 6,
+        title: "Webbutvecklare",
+        name: "Gilda",
+        mail: "mail",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    }
+];
+
+
 const About = () => {
-    return (
-        <div className="container-about">
-            <h1>The Team Behind The Website</h1>
-            <span>About Us</span>
-            <div className="contact">
-                <h2>Name</h2>
-                <span>email?</span>
-                <p>Lorem ipsum lol</p>
-            </div>
-            <div className="contact">
-                <h2>Name</h2>
-                <span>email?</span>
-                <p>Lorem ipsum lol</p>
+    return groups.map((group) => (
+        <div className="container-about" key={group.index}>
+            <h2 className="title">{group.title}</h2>
+            <h1 className="name">{group.name}</h1>
+            <h2 className="mail">{group.mail}</h2>
+            <div className="about-text">
+                <p>{group.description}</p>
             </div>
         </div>
+    )
     );
 };
 
